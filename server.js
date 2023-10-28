2c1b73ebd5b873acad892d16a237e0a157a34e64
HloniTshele <HloniTshele@gmail.com>
2023-10-28 20:51:38 +0200

    Importing handlers errors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import   express from 'express';
+import  express from 'express';
 import  bodyParser from 'body-parser';
 import  bcrypt from 'bcrypt-nodejs';
 import  cors from 'cors';
 import  knex from 'knex';
-import  register from './controllers/register.js';
-import  login from './controllers/signIn.js';
-import  image from './controllers/image.js';
-import  profile from './controllers/profile.js';
+import  handleRegister from './controllers/register.js';
+import  signInHandler from './controllers/signIn.js';
+import  imageHandler from './controllers/image.js';
+import  handleApiCall from './controllers/image.js';
+
+
 
 const db = knex({
   client: 'pg',
@@ -28,18 +30,18 @@ app.use(cors());
 
 
 //sign in POST
-app.post('/signin', (req,res)=> {login.signInHandler(req,res,db,bcrypt)} )
+app.post('/signin', (req,res)=> {signInHandler(req,res,db,bcrypt)} )
 
 //register:POST
-app.post('/register',(req,res)=> {register.handleRegister(req,res,db,bcrypt)})
+app.post('/register',(req,res)=> {handleRegister(req,res,db,bcrypt)})
 
 //getting the user data
-app.get('/profile/:id',(req,res)=>{profile.profileHandler(req,res,db)});
+app.get('/profile/:id',(req,res)=>{profileHandler(req,res,db)});
 
 
 //counting the entries /image PUT
-app.put('/image',(req,res)=> {image.imageHandler(req,res,db)})
-app.post('/imageurl',(req,res)=> {image.handleApiCall(req,res)})
+app.put('/image',(req,res)=> {imageHandler(req,res,db)})
+app.post('/imageurl',(req,res)=> {handleApiCall(req,res)})
 
 app.listen(3000, ()=>{
     console.log('app is running on port 3000')
